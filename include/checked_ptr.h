a33bb7bb826226f22b6752ef0f5d74b73625177e
jonasohland <jonas.ohland@gmail.com>
2019-02-23 09:12:19 +0100

    macos impl and optional typeid support

--- include/checked_ptr.h
+++ include/checked_ptr.h
@@ -6,6 +6,11 @@
 #include <Windows.h>
 #else
 #include <unistd.h>
+#include <fcntl.h>
+#endif
+
+#ifdef CHECKED_PTR_USE_TYPEID_HASH
+#include <typeinfo>
 #endif
 
 // https://stackoverflow.com/questions/2111667/compile-time-string-hashing
@@ -61,7 +66,8 @@ constexpr uint32_t crc32< size_t( -1 ) >( const char* str ) {
 }
 
 #define CONSTEXPR_HASH( x ) ( crc32< sizeof( x ) - 2 >( x ) ^ 0xFFFFFFFF )
-#define CONSTEXPR_TYPENAME_HASH( x ) CONSTEXPR_HASH( #x )
+#define TYPENAME_STR( x ) #x
+#define CONSTEXPR_TYPENAME_HASH( x ) CONSTEXPR_HASH( TYPENAME_STR( x ) )
 
 enum test { val = CONSTEXPR_TYPENAME_HASH( lel::kek::rofl ) };
 
@@ -74,38 +80,49 @@ enum test { val = CONSTEXPR_TYPENAME_HASH( lel::kek::rofl ) };
 
 #ifdef _WIN32
 
-bool is_readable_mem( void* mem ) {
-
+// sketchy af
+bool addr_is_readable( void* mem ) {
     MEMORY_BASIC_INFORMATION info;
     auto s = VirtualQuery( mem, &info, sizeof( info ) );
-
     return info.Protect >= PAGE_READONLY;
 }
 
 #else
 
-bool is_readable_mem( void* mem ) {
-
-    int f = open( "/dev/null", O_WRONLY );
-    int written = write( f, mem, 4 );
-    close( f );
-
+// even more sketchy
+bool addr_is_readable( void* mem ) {
+    int written = write( STDOUT_FILENO, mem, 4 );
     return written > 0;
 }
 
 #endif
 
+// #define CHECKED_PTR_USE_TEMPLATE_HASH
+
+
 template < typename T >
 struct checkable_obj {
 
-    std::ptrdiff_t validator_field_;
+    size_t validator_field_;
 
     T payload_;
+    
+    
+#ifndef CHECKED_PTR_USE_TYPEID_HASH
 
     template < typename... Args >
-    checkable_obj( std::ptrdiff_t typename_hash, Args&&... args )
-        : validator_field_( typename_hash )
+    checkable_obj( size_t&& typename_hash, Args&&... args )
+    : validator_field_( typename_hash )
         , payload_( std::forward< Args >( args )... ) {}
+    
+#else
+
+    template < typename... Args >
+    checkable_obj( Args&&... args )
+        : validator_field_( typeid( T ).hash_code() )
+        , payload_( std::forward< Args >( args )... ) {}
+
+#endif
 
     T& operator*() { return payload_; }
 
@@ -121,14 +138,54 @@ class checked_ptr {
 
     checked_ptr( void* mem )
         : memory_region_( reinterpret_cast< checkable_obj< T >* >( mem ) ) {}
+    
+#ifndef CHECKED_PTR_USE_TYPEID_HASH
 
-    bool valid( std::ptrdiff_t typename_hash ) const noexcept {
-        return is_readable_mem( const_cast< void* >(
+    bool check( size_t typename_hash ) const noexcept {
+        return addr_is_readable( const_cast< void* >(
                    reinterpret_cast< const void* >( memory_region_ ) ) ) &&
                memory_region_->validator_field_ == typename_hash;
     }
+    
+#else
+
+    bool check() const noexcept {
+        return addr_is_readable( const_cast< void* >(
+                   reinterpret_cast< const void* >( memory_region_ ) ) ) &&
+               memory_region_->validator_field_ == typeid( T ).hash_code();
+    }
+
+#endif
 
     T& operator*() { return ( *( *memory_region_ ) ); }
 
     T* operator->() { return &( *( *memory_region_ ) ); }
 };
+
+
+#ifndef CHECKED_PTR_USE_TYPEID_HASH
+
+template < typename T >
+checked_ptr< T > make_checked( void* mem, size_t check_hash ) {
+    auto ptr = checked_ptr< T >( mem );
+    if ( !ptr.check( check_hash ) )
+        throw std::runtime_error( "invalid ptr" );
+    return ptr;
+}
+
+#else
+
+template < typename T >
+checked_ptr< T > make_checked( void* mem ) {
+    auto ptr = checked_ptr< T >( mem );
+    if ( !ptr.check() )
+        throw std::runtime_error( "invalid ptr" );
+    return ptr;
+}
+
+#endif
+
+#define gen_checked_ptr( type, name, mem )                                               \
+    auto name = checked_ptr< type >( mem );                                              \
+    if ( !name.valid( CONSTEXPR_TYPENAME_HASH( type ) ) )                                \
+        throw std::runtime_error( "invalid pointer" );
